e5ee28f60b9db23efb95032764019c595ff1e528
Asher Manangan <51806917+AsherManangan@users.noreply.github.com>
2020-08-29 14:52:14 +0800

    Create pythagoreantriple.py

---
+++ beginner-projects-solutions/pythagoreantriple.py
@@ -0,0 +1,9 @@
+a = int(input ("Enter side A: "))
+b = int (input ("Enter side B: "))
+c = int (input ("Enter the size of the hypotenuse: "))
+C = c * c
+print (" \n")
+if (C == ((a*a)+(b*b))):
+	print ("Pythagorean Triple")
+else:
+	print ("Not a Pythagorean Triple")
